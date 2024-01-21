3287458ed5f588cdc2b1b77561843fe1e1f12065
freaks <mickael.sayer.dev@gmail.com>
2024-01-21 15:07:01 +0100

    test

--- assets/react/controllers/security/section/Login.jsx
+++ assets/react/controllers/security/section/Login.jsx
@@ -34,6 +34,7 @@ const Login = () => {
             }
 
             if (fortgotPassword && fortgotPassword == 0) {
+                console.log('test');
                 getFlashFunction.addCriticalError(
                     "Aïe, je ne peux pas modifier ton mot de passe. Réessaie."
                 );
